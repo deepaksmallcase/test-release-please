2fa707dc03c87d001a392f391e4e36745d8d8f27
deepaksmallcase <deepaksingh.dsh8090@gmail.com>
2025-08-10 03:59:43 +0530

    revert: remove welcome message and get started button from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,6 @@ const Home = () => {
     <div>
       <h1>Home changed now 2nd</h1>
       <div>Home section links</div>
-      <p>New feature: Welcome message added!</p>
-      <button>Get Started</button>
     </div>
   );
 };
