af178f58646a38e89df64bcdcd5eaf9b36080535
deepaksmallcase <deepaksingh.dsh8090@gmail.com>
2025-09-03 13:03:48 +0530

    fix(About): update heading text in About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>About changing this for first time</h1>
+      <h1>About changing this for second time</h1>
     </div>
   );
 };
